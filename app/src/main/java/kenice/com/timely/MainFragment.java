947e5a3b225731a76d1342f55629fcabaa5f4fbf
Kenice N <kenicenoel@outlook.com>
2016-04-01 00:26:44 -0400

    First Sync

---
+++ app/src/main/java/kenice/com/timely/MainFragment.java
@@ -0,0 +1,38 @@
+package kenice.com.timely;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MainFragment extends Fragment
+{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState)
+    {
+        Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.mainToolbar);
+        super.onActivityCreated(savedInstanceState);
+    }
+
+
+}
